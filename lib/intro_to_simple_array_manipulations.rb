f542eb4ce9f47c58fdc1cc5a0dd2d128f5e46e70
Laura Royle <lauraamie.royle@gmail.com>
2019-01-30 11:39:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,5 +11,5 @@ def using_pop(great_hits_of_the_nineties)
 end
 
 def pop_with_args(chars_in_game_of_thrones)
-  chars_arya_killed = chars_in_game_of_thrones.pop[2, 3]
+  chars_arya_killed = chars_in_game_of_thrones.pop[2]
 end
