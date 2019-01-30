401f8ecc82bfdec7552e4137c1856d8fb6aee141
Laura Royle <lauraamie.royle@gmail.com>
2019-01-30 11:51:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,3 +17,7 @@ end
 def using_shift(my_favorite_cities)
   im_so_over_this_city = my_favorite_cities.shift
 end
+
+def shift_with_args(ice_cream_brands)
+  brands_removed = ice_cream_brands.shift(2)
+end
