d57dc9ca33d70c369f769b0f0added4f3d8223c7
Laura Royle <lauraamie.royle@gmail.com>
2019-01-30 11:30:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,7 @@
 def using_push(countries_in_western_africa, next_country)
   updated_array = countries_in_western_africa.push('Niger')
 end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  updated_array = neighborhoods_in_northwest_brooklyn.unshift('Brooklyn Heights')
+end
