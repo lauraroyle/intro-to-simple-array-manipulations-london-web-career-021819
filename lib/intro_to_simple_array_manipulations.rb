98f9dba62113d965f2528471ab6eb62a1b9ce844
Laura Royle <lauraamie.royle@gmail.com>
2019-01-30 12:01:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,7 @@ end
 def using_concat(my_favorite_things, more_favs)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
+
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+  new_array = list_of_esoteric_programming_languages.insert(4, "Malbolge")
+end
