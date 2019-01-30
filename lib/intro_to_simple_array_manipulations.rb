bb82a7f67ae1d2c4dfa212aff10ed936d43e571d
Laura Royle <lauraamie.royle@gmail.com>
2019-01-30 11:27:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,3 @@
 def using_push(countries_in_western_africa, next_country)
-  updated_array = countries_in_western_africa.push('next_country')
+  updated_array = countries_in_western_africa.push('Niger')
 end
